649bfe7b56f16b765bae07f1aea772bebfc5cc73
Apollyon <37031311+JusCampin@users.noreply.github.com>
2024-09-05 20:14:25 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,20 +4,22 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 lua54 'yes'
 
-version '0.1.0'
+version '0.2.0'
 
 description 'All in one player and NPC medic script'
 
-shared_script {
+shared_scripts {
     'configs/*.lua',
     'locale.lua',
     'languages/*.lua'
 }
 
 client_scripts {
+    'client/functions.lua',
     'client/dataview.lua',
     'client/Weapons.lua',
     'client/client.lua',
+    'client/menu.lua',
 }
 
 server_scripts {
