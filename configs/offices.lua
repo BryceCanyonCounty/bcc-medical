5e0ba660518263e2576a0d0f99a2e42937622de3
Apollyon <37031311+JusCampin@users.noreply.github.com>
2024-09-15 13:48:38 -0700

    Update offices.lua
    - add office hours of operation
    - Set menu header/sub-header per location
    - added more blip options

--- configs/offices.lua
+++ configs/offices.lua
@@ -1,51 +1,141 @@
 -- Doctors Offices
 Offices = {
     valentine = {
-        coords = vector3(-288.72, 808.83, 119.39), -- Location of Doctors Office
+        office = {
+            prompt = 'Valentine Doctors Office',         -- Text Below Cabinet Prompt
+            location = vector3(-288.72, 808.83, 119.39), -- Location of Doctors Office
+            distance = 1.5,                              -- Distance Between Player and Supply Cabinet to Show Prompt
+            hours = {
+                active = false,                          -- Office uses Open and Closed Hours
+                open = 7,                                -- Office Open Time / 24 Hour Clock
+                close = 21                               -- Office Close Time / 24 Hour Clock
+            }
+        },
         blip = {
-            name = 'Doctors Office',               -- Name of Blip on Map
-            sprite = -695368421,                   -- Medical Icon
-            color = 'WHITE',                       -- Blip Color
+            show = true,             -- Show Blip On Map
+            showClosed = true,       -- Show Blip On Map when Closed (if true, 'show' must be true)
+            name = 'Doctors Office', -- Name of Blip on Map
+            sprite = -695368421,     -- Medical Icon
+            color = {
+                open = 'WHITE',      -- Office Open - Default: White - Blip Colors Shown in Main Config
+                closed = 'RED',      -- Office Closed - Deafault: Red - Blip Colors Shown in Main Config
+            },
+        },
+        menu = {
+            header = 'Medical',                      -- Header of Supply Cabinet Menu
+            subHeader = 'Supply Cabinet',            -- Sub Header of Supply Cabinet Menu
         },
     },
     -----------------------------------------------------
 
     blackwater = {
-        coords = vector3(-807.85, -1239.01, 43.56),
+        office = {
+            prompt = 'Blackwater Doctors Office',
+            location = vector3(-807.85, -1239.01, 43.56),
+            distance = 1.5,
+            hours = {
+                active = false,
+                open = 7,
+                close = 21
+            }
+        },
         blip = {
+            show = true,
+            showClosed = true,
             name = 'Doctors Office',
             sprite = -695368421,
-            color = 'WHITE',
+            color = {
+                open = 'WHITE',
+                closed = 'RED',
+            },
+        },
+        menu = {
+            header = 'Medical',
+            subHeader = 'Supply Cabinet',
         },
     },
     -----------------------------------------------------
 
     strawberry = {
-        coords = vector3(-1807.87, -430.77, 158.83),
+        office = {
+            prompt = 'Strawberry Doctors Office',
+            location = vector3(-1807.87, -430.77, 158.83),
+            distance = 1.5,
+            hours = {
+                active = false,
+                open = 7,
+                close = 21
+            }
+        },
         blip = {
+            show = true,
+            showClosed = true,
             name = 'Doctors Office',
             sprite = -695368421,
-            color = 'WHITE',
+            color = {
+                open = 'WHITE',
+                closed = 'RED',
+            },
+        },
+        menu = {
+            header = 'Medical',
+            subHeader = 'Supply Cabinet',
         },
     },
     -----------------------------------------------------
 
     stdenis = {
-        coords = vector3(2722.84, -1229.48, 50.37),
+        office = {
+            prompt = 'St. Denis Doctors Office',
+            location = vector3(2722.84, -1229.48, 50.37),
+            distance = 1.5,
+            hours = {
+                active = false,
+                open = 7,
+                close = 21
+            }
+        },
         blip = {
+            show = true,
+            showClosed = true,
             name = 'Doctors Office',
             sprite = -695368421,
-            color = 'WHITE',
+            color = {
+                open = 'WHITE',
+                closed = 'RED',
+            },
+        },
+        menu = {
+            header = 'Medical',
+            subHeader = 'Supply Cabinet',
         },
     },
     -----------------------------------------------------
 
     rhodes = {
-        coords = vector3(1372.43, -1305.73, 77.97),
+        office = {
+            prompt = 'Rhodes Doctors Office',
+            location = vector3(1372.43, -1305.73, 77.97),
+            distance = 1.5,
+            hours = {
+                active = false,
+                open = 7,
+                close = 21
+            }
+        },
         blip = {
+            show = true,
+            showClosed = true,
             name = 'Doctors Office',
             sprite = -695368421,
-            color = 'WHITE',
+            color = {
+                open = 'WHITE',
+                closed = 'RED',
+            },
+        },
+        menu = {
+            header = 'Medical',
+            subHeader = 'Supply Cabinet',
         },
     },
 }
